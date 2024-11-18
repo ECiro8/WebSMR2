f0f385115e55d0228be524b367fad55f8f101d20
ECiro8 <emma.jcarogas@gmail.com>
2024-11-18 08:42:29 +0100

    jsssssssss

--- WEB/UD4 Javascript/variables/js/clase01.js
+++ WEB/UD4 Javascript/variables/js/clase01.js
@@ -3,7 +3,7 @@ let edad2 = 20;
 
 if (edad1 > edad2){
     console.log("edad1 es MAYOR que edad2")
-} else if (edad1 < edad2){
+}else if (edad1 < edad2){
     console.log("edad1 es MENOR que edad2")
 }else if (edad1 == edad2){
     console.log("edad1 es IGUAL que edad2")
