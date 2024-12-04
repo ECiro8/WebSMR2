d9ef51f088209b5a081fcde163be0f76e516621f
ECiro8 <emma.jcarogas@gmail.com>
2024-12-04 14:07:31 +0100

    Programa DOM

---
+++ WEB/UD4 Javascript/programa/script.js
@@ -0,0 +1,21 @@
+function cparrafo(){
+    let textinho = document.querySelector(".parrafo");
+    let nombre = prompt("Introduce tu nombre");
+    textinho.textContent = nombre ;
+}
+
+function cfamarillo(){
+    document.body.style.backgroundColor = "yellow";
+}
+function cfazul(){
+    document.body.style.backgroundColor = "blue";
+}
+function cfrojo(){
+    document.body.style.backgroundColor = "red";
+}
+function cfgris(){
+    document.body.style.backgroundColor = "gray";
+}
+
+
+
