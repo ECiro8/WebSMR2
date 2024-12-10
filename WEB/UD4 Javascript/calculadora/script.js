8e8b02c98cc3504b25323738c3c5e3510477cc69
ECiro8 <emma.jcarogas@gmail.com>
2024-12-10 10:17:15 +0100

    estilo bien

---
+++ WEB/UD4 Javascript/calculadora/script.js
@@ -0,0 +1,31 @@
+
+function uno(){
+    let operacion = document.querySelector("#operasao");
+    operacion.textContent = "1";
+}
+
+function dos(){
+    let operacion = document.querySelector("#operasao");
+    operacion.textContent = "2";
+}
+
+function tres(){
+    let operacion = document.querySelector("#operasao");
+    operacion.textContent = "3";
+}
+
+function cuatro(){
+    let operacion = document.querySelector("#operasao");
+    operacion.textContent = "4";
+}
+
+function uno(){
+    let operacion = document.querySelector("#operasao");
+    operacion.textContent = "1";
+}
+
+function uno(){
+    let operacion = document.querySelector("#operasao");
+    operacion.textContent = "1";
+}
+
